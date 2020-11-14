0cd2c9e42911a34c88d1b769fcb76faf52f59945
Daron Andersonn <66185223+daron66@users.noreply.github.com>
2020-11-13 19:27:48 -0800

    Add files via upload
    First commit.

---
+++ nnjs/nodesetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#NPM and NodeJS Setup
+sudo apt-get update && sudo apt-get install npm && sudo apt-get install nodejs
+nodejs -v
+
---
+++ nnjs/nodeupdate.sh
@@ -0,0 +1,10 @@
+#Runiing this script will remove and update your current node  version
+sudo npm cache clean -f		#This line will remove the current version of NPM/Cleans Cache.
+
+sudo npm install npm@latest -g	#This line will install the latest npm version
+
+#sudo npm install -g n		#Install node helper via global after, use command below or refer to the cited work.
+
+#sudo n stable		#Uncommenting Will Install stable version of NodeJS
+
+#Cited from https://codeforgeek.com/update-node-using-npm/
